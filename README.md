fc7a3c9c86773a7f43dcd030eba83609f1372707
quochung5c <35331050+quochung5c@users.noreply.github.com>
2018-05-30 17:04:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Unnamed-Site-2
---
+++ untitled.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+var ask=prompt('Bạn có uống rượu không?');
+if (ask === 'Yes')
+	document.write('Em xin lỗi');
+if (ask === "No")
+	window.open(like.html);
